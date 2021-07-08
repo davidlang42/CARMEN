24e9aa7e2ce32c4595727c18901a2788a35a7ebe
David Lang <davidlang42@gmail.com>
2021-07-08 20:12:02 +1000

    Implement CountByGroup.Everyone

--- Model/CountByGroup.cs
+++ Model/CountByGroup.cs
@@ -13,9 +13,34 @@ namespace Model
     /// </summary>
     public class CountByGroup
     {
+        const uint DEFAULT_COUNT = 0;
+
+        #region Database fields
         internal virtual int RoleId { get; private set; }
         internal virtual int CastGroupId { get; private set; }
         public virtual CastGroup CastGroup { get; set; } = null!;
-        public uint Count { get; set; }
+        private uint? count = DEFAULT_COUNT; // null means include everyone
+        #endregion
+
+        /// <summary>The number of applicants required of this CastGroup</summary>
+        public uint Count
+        {
+            get => count ?? (uint)CastGroup.Members.Count;
+            set => count = value;
+        }
+
+        /// <summary>When true, Count is overriden to be the current number of members of this CastGroup</summary>
+        public bool Everyone
+        {
+            get => count == null;
+            set
+            {
+                if (value)
+                    count = null;
+                else if (count == null)
+                    count = DEFAULT_COUNT;
+                // else: do nothing
+            }
+        }
     }
 }
