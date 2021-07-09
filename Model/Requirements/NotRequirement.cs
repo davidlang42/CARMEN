114fb6eed61ece4a060b3277b00164d8553e658b
David Lang <davidlang42@gmail.com>
2021-07-09 18:38:07 +1000

    Add NotRequirement

---
+++ Model/Requirements/NotRequirement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Requirements
+{
+    public class NotRequirement : Requirement
+    {
+        public virtual Requirement SubRequirement { get; set; } = null!;
+
+        public NotRequirement(Requirement requirement)
+        {
+            SubRequirement = requirement;
+        }
+
+        public override bool IsSatisfiedBy(Applicant applicant)
+            => !SubRequirement.IsSatisfiedBy(applicant);
+
+        public override double SuitabilityOf(Applicant applicant)
+            => 1 - SubRequirement.SuitabilityOf(applicant);
+    }
+}
