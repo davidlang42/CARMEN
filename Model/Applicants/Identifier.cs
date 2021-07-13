1ed70d0c3778cfcb51ef42a8b331da820638050a
David Lang <davidlang42@gmail.com>
2021-07-13 22:12:01 +1000

    Add order to Identifiers

--- Model/Applicants/Identifier.cs
+++ Model/Applicants/Identifier.cs
@@ -8,11 +8,12 @@ namespace Model.Applicants
     /// <summary>
     /// The description of an identifier on which applicants can be assigned an identity.
     /// </summary>
-    public class Identifier
+    public class Identifier : IOrdered
     {
         [Key]
         public int IdentifierId { get; private set; }
         public string Name { get; set; } = "";
+        public int Order { get; set; }
         public string DefaultPrefix { get; set; } = "";
         public string DefaultSuffix { get; set; } = "";
         public virtual ICollection<Requirement> Requirements { get; private set; } = new ObservableCollection<Requirement>();
