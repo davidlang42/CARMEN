27fb3cdd82a3dbd79239141cf3e4934c5c327c1c
David Lang <davidlang42@gmail.com>
2021-07-15 00:00:03 +1000

    Finish implementing EditableListControl, including moving items and EditableListWrapper

--- DatabaseExplorer/EditableListControl.xaml.cs
+++ DatabaseExplorer/EditableListControl.xaml.cs
@@ -40,35 +40,52 @@ namespace DatabaseExplorer
 
         private void optionsMoveUp_Click(object sender, RoutedEventArgs e)
         {
-            //TODO implement move up
+            if (optionsList.SelectedItem != null && optionsList.SelectedIndex != 0)
+                MoveItem(optionsList.SelectedIndex, optionsList.SelectedIndex - 1);
         }
 
         private void optionsDelete_Click(object sender, RoutedEventArgs e)
         {
             if (ItemsSource == null || optionsList.SelectedItem == null)
                 return;
-            if (ItemsSource.IsFixedSize || ItemsSource.IsReadOnly)
-                ItemsSource = Enumerable.Range(0, ItemsSource.Count).Zip(ItemsSource.Cast<string>())
-                    .Where(p => p.First != optionsList.SelectedIndex).Select(p => p.Second).ToArray();
-            else
-                ItemsSource.RemoveAt(optionsList.SelectedIndex);
-            //TODO select item at removed index
+            var index = optionsList.SelectedIndex;
+            using (var list = new EditableListWrapper<string>(ItemsSource, new_list => ItemsSource = new_list))
+                list.RemoveAt(index);
+            optionsList.SelectedIndex = index;
+            optionsNewItem.Focus();
         }
 
         private void optionsMoveDown_Click(object sender, RoutedEventArgs e)
         {
-            //TODO implement move down
+            if (optionsList.SelectedItem != null && optionsList.SelectedIndex != optionsList.Items.Count - 1)
+                MoveItem(optionsList.SelectedIndex, optionsList.SelectedIndex + 1);
+        }
+
+        private void MoveItem(int from_index, int to_index)
+        {
+            if (ItemsSource == null)
+                return;
+            var value = optionsList.Items[from_index];
+            using (var list = new EditableListWrapper<string>(ItemsSource, new_list => ItemsSource = new_list))
+            {
+                list.RemoveAt(from_index);
+                list.Insert(to_index, value);
+            }
+            optionsList.SelectedIndex = to_index;
         }
 
         private void optionsAdd_Click(object sender, RoutedEventArgs e)
         {
             if (ItemsSource == null)
                 return;
-            if (ItemsSource.IsFixedSize || ItemsSource.IsReadOnly)
-                ItemsSource = ItemsSource.Cast<string>().Concat(new[] { optionsNewItem.Text }).ToArray(); //TODO insert before selected item (if selected), that way when something is double clicked to edit, and then you hit enter, it goes back where it was
-            else
-                ItemsSource.Add(optionsNewItem.Text);
+            var value = optionsNewItem.Text;
+            using (var list = new EditableListWrapper<string>(ItemsSource, new_list => ItemsSource = new_list))
+                if (optionsList.SelectedIndex < 0)
+                    list.Add(value);
+                else
+                    list.Insert(optionsList.SelectedIndex, value);
             optionsNewItem.Text = "";
+            optionsNewItem.Focus();
         }
 
         private void optionsNewItem_KeyDown(object sender, KeyEventArgs e)
@@ -83,7 +100,60 @@ namespace DatabaseExplorer
                 return;
             optionsNewItem.Text = optionsList.SelectedItem.ToString();
             optionsDelete_Click(sender, new RoutedEventArgs());
-            optionsNewItem.Focus();
+            optionsNewItem.SelectAll();
+        }
+
+        private void optionsList_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Delete)
+                optionsDelete_Click(sender, new RoutedEventArgs());
+        }
+    }
+
+    public class EditableListWrapper<T> : IList, IDisposable
+    {
+        private IList workingList;
+        private Action? setter;
+
+        public EditableListWrapper(IList source_list, Action<IList> setter)
+        {
+            if (source_list.IsReadOnly || source_list.IsFixedSize)
+            {
+                // list will be replaced on dispose
+                var new_list = source_list.OfType<T>().ToList();
+                workingList = new_list;
+                this.setter = () => setter(new_list.ToArray());
+            }
+            else
+            {
+                // list will be modified directly
+                workingList = source_list;
+                this.setter = null;
+            }
+        }
+
+        #region Implement IList through workingList
+        public object? this[int index] { get => workingList[index]; set => workingList[index] = value; }
+        public bool IsFixedSize => workingList.IsFixedSize;
+        public bool IsReadOnly => workingList.IsReadOnly;
+        public int Count => workingList.Count;
+        public bool IsSynchronized => workingList.IsSynchronized;
+        public object SyncRoot => workingList.SyncRoot;
+        public int Add(object? value) => workingList.Add(value);
+        public void Clear() => workingList.Clear();
+        public bool Contains(object? value) => workingList.Contains(value);
+        public void CopyTo(Array array, int index) => workingList.CopyTo(array, index);
+        public IEnumerator GetEnumerator() => workingList.GetEnumerator();
+        public int IndexOf(object? value) => workingList.IndexOf(value);
+        public void Insert(int index, object? value) => workingList.Insert(index, value);
+        public void Remove(object? value) => workingList.Remove(value);
+        public void RemoveAt(int index) => workingList.RemoveAt(index);
+        #endregion
+
+        public void Dispose()
+        {
+            setter?.Invoke();
+            setter = null;
         }
     }
 }
