603c762824cc34a6bded089d447c2492d8bfc441
David Lang <davidlang42@gmail.com>
2021-07-24 01:09:20 +1000

    Save & restore window settings

--- CarmenUI/App.xaml.cs
+++ CarmenUI/App.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Configuration;
 using System.Data;
 using System.Linq;
@@ -13,5 +13,26 @@ namespace CarmenUI
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            CarmenUI.Properties.Settings.Default.Save();
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            if (CarmenUI.Properties.Settings.Default.FirstRun)
+                SetDefaultSettings();
+        }
+
+        public void SetDefaultSettings()
+        {
+            var settings = CarmenUI.Properties.Settings.Default;
+            settings.FirstRun = false;
+            settings.Width = 1024;
+            settings.Height = 768;
+            settings.Left = (SystemParameters.PrimaryScreenWidth - settings.Width) / 2;
+            settings.Top = (SystemParameters.PrimaryScreenHeight - settings.Height) / 2;
+            settings.WindowState = WindowState.Normal;
+        }
     }
 }
---
+++ CarmenUI/SettingBindingExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace CarmenUI
+{
+    /// <summary>
+    /// A two-way binding to the settings properties.
+    /// SOURCE: https://thomaslevesque.com/2008/11/18/wpf-binding-to-application-settings-using-a-markup-extension/
+    /// </summary>
+    public class SettingBindingExtension : Binding
+    {
+        public SettingBindingExtension()
+        {
+            Initialize();
+        }
+
+        public SettingBindingExtension(string path)
+            : base(path)
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            Source = Properties.Settings.Default;
+            Mode = BindingMode.TwoWay;
+        }
+    }
+}
