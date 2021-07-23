2151445ba16cd9332b612f1791bd4e8dcf1c454d
David Lang <davidlang42@gmail.com>
2021-07-24 03:39:14 +1000

    Add LoadWindow with icons

--- CarmenUI/LoadWindow.xaml.cs
+++ CarmenUI/LoadWindow.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace CarmenUI
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for LoadDatabase.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoadDatabase : Window
     {
-        public MainWindow()
+        public LoadDatabase()
         {
             InitializeComponent();
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            var x = new LoadDatabase();
-            x.ShowDialog();
         }
     }
 }
