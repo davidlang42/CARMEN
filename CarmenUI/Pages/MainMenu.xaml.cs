62ec08898218f9900590c7bd48d99b6a3f6e26ec
David Lang <davidlang42@gmail.com>
2021-07-24 23:01:12 +1000

    Add blank pages

--- CarmenUI/Pages/MainMenu.xaml.cs
+++ CarmenUI/Pages/MainMenu.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,63 @@ namespace CarmenUI.Pages
         public MainMenu()
         {
             InitializeComponent();
+        }
+
+        private void ConfigureShow_Selected(object sender, RoutedEventArgs e)
+            => this.NavigationService.Navigate(new Uri("/Pages/ConfigureShow.xaml", UriKind.Relative));
+
+        private void ConfigureShow_MouseEnter(object sender, MouseEventArgs e)
+        {
+            UpdateSummaryPanel("Summary panel for configuring the show.");
+        }
+
+        private void RegisterApplicants_Selected(object sender, RoutedEventArgs e)
+            => this.NavigationService.Navigate(new Uri("/Pages/Applicants.xaml", UriKind.Relative));
+
+        private void RegisterApplicants_MouseEnter(object sender, MouseEventArgs e)
+        {
+            UpdateSummaryPanel("Summary panel for registering applicants.");
+        }
+
+        private void AuditionApplicants_Selected(object sender, RoutedEventArgs e)
+            => this.NavigationService.Navigate(new Uri("/Pages/Applicants.xaml", UriKind.Relative));
+
+        private void AuditionApplicants_MouseEnter(object sender, MouseEventArgs e)
+        {
+            UpdateSummaryPanel("Summary panel for auditioning applicants.");
+        }
+
+        private void SelectCast_Selected(object sender, RoutedEventArgs e)
+            => this.NavigationService.Navigate(new Uri("/Pages/SelectCast.xaml", UriKind.Relative));
+
+        private void SelectCast_MouseEnter(object sender, MouseEventArgs e)
+        {
+            UpdateSummaryPanel("Summary panel for selecting cast.");
+        }
+
+        private void ConfigureItems_Selected(object sender, RoutedEventArgs e)
+            => this.NavigationService.Navigate(new Uri("/Pages/ConfigureItems.xaml", UriKind.Relative));
+
+        private void ConfigureItems_MouseEnter(object sender, MouseEventArgs e)
+        {
+            UpdateSummaryPanel("Summary panel for configuring items and roles.");
+        }
+
+        private void AllocateRoles_Selected(object sender, RoutedEventArgs e)
+            => this.NavigationService.Navigate(new Uri("/Pages/AllocateRoles.xaml", UriKind.Relative));
+
+        private void AllocateRoles_MouseEnter(object sender, MouseEventArgs e)
+        {
+            UpdateSummaryPanel("Summary panel for allocating roles.");
+        }
+
+        private void UpdateSummaryPanel(string text)
+        {
+            SummaryPanel.Children.Clear();
+            SummaryPanel.Children.Add(new TextBlock
+            {
+                Text = text
+            });
         }
     }
 }
