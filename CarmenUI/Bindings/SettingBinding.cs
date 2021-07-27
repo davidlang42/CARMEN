43107ab67d78622feed0e53beb4a2e40d7a29c31
David Lang <davidlang42@gmail.com>
2021-07-28 01:01:43 +1000

    Clean up SettingBinding

--- CarmenUI/Bindings/SettingBinding.cs
+++ CarmenUI/Bindings/SettingBinding.cs
@@ -5,20 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
 
-namespace CarmenUI
+namespace CarmenUI.Bindings
 {
     /// <summary>
     /// A two-way binding to the settings properties.
     /// SOURCE: https://thomaslevesque.com/2008/11/18/wpf-binding-to-application-settings-using-a-markup-extension/
     /// </summary>
-    public class SettingBindingExtension : Binding
+    public class SettingBinding : Binding
     {
-        public SettingBindingExtension()
+        public SettingBinding()
         {
             Initialize();
         }
 
-        public SettingBindingExtension(string path)
+        public SettingBinding(string path)
             : base(path)
         {
             Initialize();
