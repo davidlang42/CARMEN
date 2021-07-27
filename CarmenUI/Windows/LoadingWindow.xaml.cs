b0a8bf211e52f5e5cc26071003b38f6d65c3ea6e
David Lang <davidlang42@gmail.com>
2021-07-28 00:15:25 +1000

    Make LoadingWindow disable owner window while open

--- CarmenUI/Windows/LoadingWindow.xaml.cs
+++ CarmenUI/Windows/LoadingWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
@@ -23,6 +25,7 @@ namespace CarmenUI.Windows
         {
             Owner = owner;
             owner.IsEnabled = false;
+            WinUser.EnableWindow(Owner, false);
             InitializeComponent();
             Show();
         }
@@ -32,6 +35,7 @@ namespace CarmenUI.Windows
 
         public void Dispose()
         {
+            WinUser.EnableWindow(Owner, true);
             Owner.IsEnabled = true;
             Hide();
         }
---
+++ CarmenUI/Windows/WinUser.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace CarmenUI.Windows
+{
+    /// <summary>
+    /// Static calls to external windows functions (Winuser.h)
+    /// </summary>
+    public static class WinUser
+    {
+        // In Winuser.h terms, "long" means 32-bit integer, which is called "int" in C#
+
+        /// <summary>Reference: https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-enablewindow </summary>
+        [DllImport("user32.dll")]
+        public static extern bool EnableWindow(IntPtr hWnd, bool bEnable);
+
+        /// <summary>Reference: https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getwindowlongptra </summary>
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowLongPtr(IntPtr hWnd, int nIndex);
+
+        /// <summary>Reference: https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-setwindowlongptra </summary>
+        [DllImport("user32.dll")]
+        public static extern IntPtr SetWindowLongPtr(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+
+        #region Window styles (https://docs.microsoft.com/en-us/windows/win32/winmsg/window-styles)
+        public const int GWL_STYLE = -16;//TODO make these into enums
+        public const int WS_DLGFRAME = 0x00400000;
+        public const int WS_MAXIMIZEBOX = 0x10000;
+        public const int WS_MINIMIZEBOX = 0x20000;
+        #endregion
+
+        #region Extended window styles (https://docs.microsoft.com/en-us/windows/win32/winmsg/extended-window-styles)
+        public const int GWL_EXSTYLE = -20;
+        public const int WS_EX_DLGMODALFRAME = 0x00000001;
+        public const int WS_EX_STATICEDGE = 0x00020000;
+        public const int WS_EX_NOACTIVATE = 0x08000000;
+        public const int WS_EX_TOPMOST = 0x00000008;
+        #endregion
+
+        #region Helper functions
+        public static bool AddWindowFlag(Window window, int nIndex, int flag)
+            => AddWindowFlag(GetHwnd(window), nIndex, flag);
+
+        public static bool AddWindowFlag(IntPtr hWnd, int nIndex, int flag)
+        {
+            var new_value = new IntPtr(GetWindowLongPtr(hWnd, nIndex).ToInt32() | flag);
+            var result = SetWindowLongPtr(hWnd, nIndex, new_value);
+            return result.ToInt32() != 0;
+        }
+
+        public static bool RemoveWindowFlag(Window window, int nIndex, int flag)
+            => RemoveWindowFlag(GetHwnd(window), nIndex, flag);
+
+        public static bool RemoveWindowFlag(IntPtr hWnd, int nIndex, int flag)
+        {
+            var new_value = new IntPtr(GetWindowLongPtr(hWnd, nIndex).ToInt32() & ~flag);
+            var result = SetWindowLongPtr(hWnd, nIndex, new_value);
+            return result.ToInt32() != 0;
+        }
+
+        public static bool EnableWindow(Window window, bool bEnable)
+            => EnableWindow(GetHwnd(window), bEnable);
+
+        public static IntPtr GetHwnd(Window window)
+        {
+            var handle = new WindowInteropHelper(window).Handle;
+            if (handle == IntPtr.Zero)
+                throw new InvalidOperationException("Could not get window handle, wait until window is fully initialised.");
+            return handle;
+        }
+        #endregion
+    }
+}
