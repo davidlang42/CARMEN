8eb17322181a536f944f32466e1371af18b2452e
David Lang <davidlang42@gmail.com>
2021-08-12 19:06:38 +1000

    Log & show unhandled exceptions

--- CarmenUI/App.xaml.cs
+++ CarmenUI/App.xaml.cs
@@ -8,6 +8,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.IO;
+using System.Text.Json;
 
 namespace CarmenUI
 {
@@ -38,5 +40,50 @@ namespace CarmenUI
             settings.WindowState = WindowState.Normal;
             settings.RecentShows = new List<RecentShow>();
         }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+#if !DEBUG
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+                HandleUnhandledException((Exception)e.ExceptionObject, "AppDomain");
+            DispatcherUnhandledException += (s, e) =>
+            {
+                HandleUnhandledException(e.Exception, "Dispatcher");
+                e.Handled = true;
+            };
+            TaskScheduler.UnobservedTaskException += (s, e) =>
+            {
+                HandleUnhandledException(e.Exception, "TaskScheduler");
+                e.SetObserved();
+            };
+#endif
+        }
+
+#if !DEBUG
+        private void HandleUnhandledException(Exception ex, string handler)
+        {
+            var path = $"{AppDomain.CurrentDomain.BaseDirectory}\\Logs";
+            Directory.CreateDirectory(path);
+            var filename = $"{path}\\Error_{DateTime.Now:yyyy-MM-dd_HH-mm-ss_fffffff}.log";
+            using (var file = File.CreateText(filename))
+                file.WriteLine($"Handler: {handler}\n{ExceptionString(ex)}");
+            MessageBox.Show($"Error in {handler}: {ex.Message}");
+        }
+
+        private string ExceptionString(Exception ex)
+        {
+            var str = ex.ToString();
+            if (ex.Data is IDictionary data && data.Count > 0)
+            {
+                str += "\nData:";
+                foreach (var key in data.Keys)
+                    str += $"\n{key}={data[key]}";
+            }
+            if (ex.InnerException is Exception inner)
+                str += $"\nInner:\n{ExceptionString(inner)}";
+            return str;
+        }
+#endif
     }
 }
