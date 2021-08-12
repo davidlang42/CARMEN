7e18433b071c996fd824aa74e44447e1e84c40b5
David Lang <davidlang42@gmail.com>
2021-08-13 02:48:26 +1000

    Skeleton of EditableImage control

---
+++ CarmenUI/UserControls/EditableImage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CarmenUI.UserControls
+{
+    /// <summary>
+    /// Interaction logic for EditableImage.xaml
+    /// </summary>
+    public partial class EditableImage : UserControl
+    {
+        public static readonly DependencyProperty ImageBytesProperty = DependencyProperty.Register(
+           nameof(ImageBytes), typeof(IList<byte>), typeof(EditableImage), new PropertyMetadata(null));
+
+        public IList<byte>? ImageBytes
+        {
+            get => (IList<byte>?)GetValue(ImageBytesProperty);
+            set => SetValue(ImageBytesProperty, value);
+        }
+
+        public EditableImage()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseEnter(object sender, MouseEventArgs e)
+            => editPencil.Visibility = Visibility.Visible;
+
+        private void Grid_MouseLeave(object sender, MouseEventArgs e)
+            => editPencil.Visibility = Visibility.Hidden;
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void UploadImage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PasteImage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
