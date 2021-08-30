f0193d9f829da5519caa4866929e2feaa9409ac6
David Lang <davidlang42@gmail.com>
2021-08-30 19:29:16 +1000

    Move existing unit tests into Model

--- UnitTests/Model/AlternativeCastTests.cs
+++ UnitTests/Model/AlternativeCastTests.cs
@@ -6,7 +6,7 @@ using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-namespace UnitTests
+namespace UnitTests.Model
 {
     public class AlternativeCastTests
     {
