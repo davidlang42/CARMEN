e2a9ffc432ee87ffc2c55a261f10e60774b9e9b2
David Lang <davidlang42@gmail.com>
2021-09-02 17:14:01 +1000

    Implement WeightedSumEngine

---
+++ CastingEngine/Heuristic/WeightedSumEngine.cs
@@ -0,0 +1,31 @@
+﻿using Carmen.ShowModel.Applicants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carmen.CastingEngine.Heuristic
+{
+    public class WeightedSumEngine : IApplicantEngine
+    {
+        /// Calculate the overall ability of an Applicant as a simple weighted sum of their Abilities</summary>
+        public int OverallAbility(Applicant applicant)
+        {
+            try
+            {
+                return Convert.ToInt32(applicant.Abilities.Sum(a => (double)a.Mark / a.Criteria.MaxMark * a.Criteria.Weight));
+            }
+            catch (OverflowException)
+            {
+                //LATER log exception
+                return int.MaxValue;
+            }
+            catch (DivideByZeroException)
+            {
+                //LATER log exception
+                return 0;
+            }
+        }
+    }
+}
