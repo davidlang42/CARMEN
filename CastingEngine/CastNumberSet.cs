954dce4eda8c9d46cf1a3128c672be49bb6e6820
David Lang <davidlang42@gmail.com>
2021-09-02 23:51:47 +1000

    Implement most of OriginalHeuristicEngine, including CastNumberSet

---
+++ CastingEngine/CastNumberSet.cs
@@ -0,0 +1,102 @@
+﻿using Carmen.ShowModel.Applicants;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carmen.CastingEngine
+{
+    /// <summary>
+    /// A set of cast numbers stored in a dictionary-based structure, with some special handling:
+    /// - A cast number allocated to null AlternativeCast cannot be allocated to anyone else
+    /// - A cast number allocated to an AlternativeCast can be allocated to other AlternativeCasts, as long as they are the same CastGroup
+    /// NOTE: Currently supports Add/Clear operations, but not Remove
+    /// </summary>
+    public class CastNumberSet
+    {
+        public const int FIRST_CAST_NUMBER = 1;
+
+        /// <summary>Stores the actual cast number data</summary>
+        Dictionary<int, CastNumberRecord> numbers { get; init; } = new();
+
+        /// <summary>The lowest cast number which is not allocated for a given AlternativeCast.
+        /// NOTE: denotes a starting point only, not guarenteed to be available</summary>
+        Dictionary<AlternativeCast, int> lowestAvailable { get; init; } = new();
+
+        /// <summary>The lowest cast number which is not allocated to anyone.
+        /// NOTE: denotes a starting point only, not guarenteed to be available</summary>
+        int lowestUnusedNumber = FIRST_CAST_NUMBER;
+
+        public bool Add(int cast_number, AlternativeCast? alternative_cast, CastGroup cast_group)
+        {
+            if (numbers.TryGetValue(cast_number, out var record))
+            {
+                if (record.CastGroup != cast_group)
+                    return false; // wrong CastGroup, so we can't share this allocation
+                if (record.AlternativeCasts is not HashSet<AlternativeCast> record_alternative_casts)
+                    return false; // cast number is fully allocated
+                if (alternative_cast == null)
+                    return false; // cast number is partially allocated, but we needed full
+                // take the partial allocation we need, or fail because that partial is already taken
+                return record_alternative_casts.Add(alternative_cast); 
+            }
+            else
+            {
+                // the full allocation is available, so we take it
+                numbers.Add(cast_number, new CastNumberRecord
+                {
+                    CastGroup = cast_group,
+                    AlternativeCasts = alternative_cast?.Yield().ToHashSet()
+                });
+                return true;
+            }
+        }
+
+        /// <summary>Find the next cast number available for a given AlternativeCast and CastGroup</summary>
+        public int AddNextAvailable(AlternativeCast? alternative_cast, CastGroup cast_group)
+        {
+            int cast_number;
+            if (alternative_cast == null)
+                cast_number = FastForwardToLowestFullyAvailable();
+            else
+                cast_number = FastForwardToLowestPartiallyAvailable(alternative_cast);
+            while (!Add(cast_number, alternative_cast, cast_group))
+                cast_number++;
+            return cast_number;
+        }
+
+        private int FastForwardToLowestPartiallyAvailable(AlternativeCast partial_required)
+        {
+            int cast_number = lowestAvailable.TryGetValue(partial_required, out var previous_lowest) ? previous_lowest : FIRST_CAST_NUMBER;
+            while (numbers.TryGetValue(cast_number, out var record) && // while some allocation exists
+                    (record.AlternativeCasts is not HashSet<AlternativeCast> existing // and it is a full allocation
+                    || existing.Contains(partial_required))) // or it contains the partial we want
+                cast_number++; // fast-forward
+            return lowestAvailable[partial_required] = cast_number;
+        }
+
+        private int FastForwardToLowestFullyAvailable()
+        {
+            int cast_number = lowestUnusedNumber;
+            while (numbers.ContainsKey(cast_number)) // while any allocation exists
+                cast_number++; // fast-foward
+            return lowestUnusedNumber = cast_number;
+        }
+
+        public void Clear()
+        {
+            numbers.Clear();
+            lowestAvailable.Clear();
+            lowestUnusedNumber = FIRST_CAST_NUMBER;
+        }
+    }
+
+    public struct CastNumberRecord
+    {
+        public CastGroup CastGroup { get; init; }
+        public HashSet<AlternativeCast>? AlternativeCasts { get; init; }
+    }
+}
---
+++ CastingEngine/Heuristic/OriginalHeuristicEngine.cs
@@ -0,0 +1,136 @@
+﻿using Carmen.ShowModel.Applicants;
+using Carmen.ShowModel.Criterias;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carmen.CastingEngine.Heuristic
+{
+    public class OriginalHeuristicEngine : WeightedSumEngine, ISelectionEngine
+    {
+        public Criteria? CastNumberOrderBy { get; set; }
+        public ListSortDirection CastNumberOrderDirection { get; set; }
+
+        /// <summary>The original heuristic engine does things in an abnormal way,
+        /// which requires these values before they would otherwise be supplied</summary>
+        public OriginalHeuristicEngine(Criteria? cast_number_order_by, ListSortDirection cast_number_order_direction)
+        {
+            CastNumberOrderBy = cast_number_order_by;
+            CastNumberOrderDirection = cast_number_order_direction;
+        }
+
+        public void SelectCastGroups(IEnumerable<Applicant> applicants, IEnumerable<CastGroup> cast_groups, uint number_of_alternative_casts)
+        {
+            var remaining_groups = new Dictionary<CastGroup, uint?>();
+            foreach (var cast_group in cast_groups)
+            {
+                if (cast_group.RequiredCount is uint required)
+                {
+                    if (cast_group.AlternateCasts)
+                        required *= number_of_alternative_casts;
+                    required -= (uint)cast_group.Members.Count;
+                    if (required > 0)
+                        remaining_groups.Add(cast_group, required);
+                }
+                else
+                    remaining_groups.Add(cast_group, null);
+            }
+            foreach (var applicant in applicants.Where(a => !a.IsAccepted).OrderByDescending(a => OverallAbility(a)))
+            {
+                if (NextAvailableCastGroup(remaining_groups, applicant) is CastGroup cg)
+                {
+                    applicant.CastGroup = cg;
+                    if (remaining_groups[cg] is uint remaining_count)
+                    {
+                        if (remaining_count == 1)
+                            remaining_groups.Remove(cg);
+                        else
+                            remaining_groups[cg] = remaining_count - 1;
+                    }
+                }
+                if (remaining_groups.Count == 0)
+                    break;
+            }
+        }
+
+        private CastGroup? NextAvailableCastGroup(Dictionary<CastGroup, uint?> remaining_groups, Applicant applicant)
+        {
+            foreach (var (cg, remaining_count) in remaining_groups)
+            {
+                if (cg.Requirements.All(r => r.IsSatisfiedBy(applicant)))
+                    return cg;
+            }
+            return null;
+        }
+
+        public void BalanceAlternativeCasts(IEnumerable<Applicant> applicants, AlternativeCast[] alternative_casts, IEnumerable<SameCastSet> _)
+        {
+            //TODO ModifiedHeuristicEngine should take into account locked sets
+            foreach (var applicants_group in applicants.GroupBy(a => a.CastGroup))
+            {
+                if (applicants_group.Key is not CastGroup cast_group // not accepted into cast
+                    || !cast_group.AlternateCasts) // non-alternating cast group
+                {
+                    // alternative cast not required, set to null
+                    foreach (var applicant in applicants_group)
+                        applicant.AlternativeCast = null;
+                }
+                else
+                {
+                    // allocate alternative cast
+                    //TODO currently this overwrites, but it should respect
+                    int next_cast = 0;
+                    foreach (var applicant in Sort(applicants, CastNumberOrderBy, CastNumberOrderDirection))
+                    {
+                        applicant.AlternativeCast = alternative_casts[next_cast++];
+                        if (next_cast == alternative_casts.Length)
+                            next_cast = 0;
+                    }
+                }
+            }
+        }
+
+        private IEnumerable<Applicant> Sort(IEnumerable<Applicant> applicants, Criteria? by, ListSortDirection direction)
+            => (by, direction) switch
+            {
+                (Criteria c, ListSortDirection.Ascending) => applicants.OrderBy(a => a.MarkFor(c)),
+                (Criteria c, ListSortDirection.Descending) => applicants.OrderByDescending(a => a.MarkFor(c)),
+                (null, ListSortDirection.Ascending) => applicants.OrderBy(a => OverallAbility(a)),
+                (null, ListSortDirection.Descending) => applicants.OrderByDescending(a => OverallAbility(a)),
+                _ => throw new ApplicationException($"Sort not handled: {by} / {direction}")
+            };
+
+        public void AllocateCastNumbers(IEnumerable<Applicant> applicants, AlternativeCast[] alternative_casts, Criteria? order_by, ListSortDirection sort_direction)
+        {
+            var cast_numbers = new CastNumberSet();
+            // find cast numbers which are already set
+            foreach (var applicant in applicants)
+            {
+                if (applicant.CastGroup != null)
+                {
+                    if (applicant.CastNumber is int cast_number)
+                        if (!cast_numbers.Add(cast_number, applicant.AlternativeCast, applicant.CastGroup))
+                            // if add fails, this cast number has already been allocated, therefore remove it
+                            applicant.CastNumber = null;
+                }
+                else
+                {
+                    // clear cast numbers of rejected applicants
+                    applicant.CastNumber = null;
+                }
+            }
+            // allocate cast numbers to those who need them
+            foreach (var applicant in Sort(applicants.Where(a => a.IsAccepted), order_by, sort_direction))
+            {
+                if (applicant.CastNumber == null)
+                    applicant.CastNumber = cast_numbers.AddNextAvailable(applicant.AlternativeCast, applicant.CastGroup!); // not null because IsAccepted
+            }
+        }
+
+        //TODO ModifiedHeuristicEngine should fallback to OverallAbility if tied on tag requirements
+        public void ApplyTags(IEnumerable<Applicant> applicants, IEnumerable<Tag> tags) => throw new NotImplementedException();
+    }
+}
