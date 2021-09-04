dc9f353ab7fef5afe70ab6fe1d519d12b465a709
David Lang <davidlang42@gmail.com>
2021-09-04 17:45:04 +1000

    Clean up WeightedSumEngine

--- CastingEngine/Heuristic/WeightedSumEngine.cs
+++ CastingEngine/Heuristic/WeightedSumEngine.cs
@@ -8,14 +8,16 @@ using System.Threading.Tasks;
 
 namespace Carmen.CastingEngine.Heuristic
 {
-    public class WeightedSumEngine : IApplicantEngine
+    public class WeightedSumEngine : ApplicantEngine
     {
-        public int MaxOverallAbility { get; init; }
+        readonly int maxOverallAbility;
+        public override int MaxOverallAbility => maxOverallAbility;
 
-        public int MinOverallAbility { get; init; }
+        readonly int minOverallAbility;
+        public override int MinOverallAbility => minOverallAbility;
 
         /// Calculate the overall ability of an Applicant as a simple weighted sum of their Abilities</summary>
-        public int OverallAbility(Applicant applicant)
+        public override int OverallAbility(Applicant applicant)
             => Convert.ToInt32(applicant.Abilities.Sum(a => (double)a.Mark / a.Criteria.MaxMark * a.Criteria.Weight));
         
         public WeightedSumEngine(Criteria[] criterias)
@@ -23,13 +25,13 @@ namespace Carmen.CastingEngine.Heuristic
             var max = criterias.Select(c => c.Weight).Where(w => w > 0).Sum();
             if (max > int.MaxValue)
                 throw new ApplicationException($"Sum of positive Criteria weights cannot exceed {int.MaxValue}: {max}");
-            MaxOverallAbility = Convert.ToInt32(max);
+            maxOverallAbility = Convert.ToInt32(max);
             var min = criterias.Select(c => c.Weight).Where(w => w < 0).Sum();
             if (min < int.MinValue)
                 throw new ApplicationException($"Sum of negative Criteria weights cannot go below {int.MinValue}: {min}");
-            MinOverallAbility = Convert.ToInt32(min);
-            if (MinOverallAbility == MaxOverallAbility) // == 0
-                MaxOverallAbility = 1; // to avoid division by zero errors
+            minOverallAbility = Convert.ToInt32(min);
+            if (minOverallAbility == maxOverallAbility) // == 0
+                maxOverallAbility = 1; // to avoid division by zero errors
         }
     }
 }
