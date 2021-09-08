5777d6ea7918ee31727bc3462aa71c00a3b420bb
David Lang <davidlang42@gmail.com>
2021-09-09 02:29:46 +1000

    Refactor common code from PairsSatEngine into SatEngine

--- CastingEngine/SAT/PairsSatEngine.cs
+++ CastingEngine/SAT/PairsSatEngine.cs
@@ -34,55 +34,19 @@ namespace Carmen.CastingEngine.SAT
             public bool Solved;
         }
 
-        private Criteria[] primaryCriterias;
-
         // expression building is O(2^n) and will be slow for large numbers, therefore cache
-        Dictionary<int, ExpressionBuilder> cachedKeepTogether = new();
         Dictionary<int, ExpressionBuilder> cachedKeepSeparate = new();
 
         public PairsSatEngine(IApplicantEngine applicant_engine, AlternativeCast[] alternative_casts, Criteria? cast_number_order_by, ListSortDirection cast_number_order_direction, Criteria[] criterias)
-            : base(applicant_engine, alternative_casts, cast_number_order_by, cast_number_order_direction)
-        {
-            primaryCriterias = criterias.Where(c => c.Primary).ToArray();
-        }
+            : base(applicant_engine, alternative_casts, cast_number_order_by, cast_number_order_direction, criterias)
+        { }
 
-        public override void BalanceAlternativeCasts(IEnumerable<Applicant> applicants, IEnumerable<SameCastSet> same_cast_sets)
+        protected override Solution FindSatSolution(List<(CastGroup, HashSet<Applicant>)> applicants_needing_alternative_cast,
+            List<Clause<Applicant>> existing_assignments, List<Clause<Applicant>> same_cast_clauses, Dictionary<Applicant, SameCastSet> same_cast_lookup,
+            out Solver<Applicant> sat)
         {
-            if (alternativeCasts.Length != 2)
-                throw new ApplicationException("The Chunked-Pairs approach only works for exactly 2 alternative casts.");
-            // sort the applicants into cast groups
-            var applicants_needing_alternative_cast = new List<(CastGroup, HashSet<Applicant>)>();
-            var existing_assignments = new List<Clause<Applicant>>();
-            foreach (var applicants_group in applicants.GroupBy(a => a.CastGroup))
-            {
-                if (applicants_group.Key is not CastGroup cast_group // not accepted into cast
-                    || !cast_group.AlternateCasts) // non-alternating cast group
-                {
-                    // alternative cast not required, set to null
-                    foreach (var applicant in applicants_group)
-                        applicant.AlternativeCast = null;
-                }
-                else
-                {
-                    var applicants_set = applicants_group.ToHashSet();
-                    applicants_needing_alternative_cast.Add((cast_group, applicants_set));
-                    // add clauses for applicants with alternative cast already set
-                    foreach (var applicant in applicants_set)
-                        if (applicant.AlternativeCast != null)
-                            existing_assignments.Add(Clause<Applicant>.Unit(applicant, applicant.AlternativeCast == alternativeCasts[1]));
-                }
-            }
-            // create clauses for same cast sets
-            var same_cast_clauses = new List<Clause<Applicant>>();
-            var same_cast_lookup = new Dictionary<Applicant, SameCastSet>();
-            foreach (var same_cast_set in same_cast_sets)
-            {
-                foreach (var applicant in same_cast_set.Applicants)
-                    same_cast_lookup.Add(applicant, same_cast_set); // Applicants can only be in one SameCastSet
-                same_cast_clauses.AddRange(KeepTogether(same_cast_set.Applicants.Where(a => applicants_needing_alternative_cast.Any(p => p.Item2.Contains(a))).ToArray()));
-            }
             // Initialise() the starting conditions, then Simplify() until the SAT solver succeeds
-            var sat = new DpllSolver<Applicant>(applicants_needing_alternative_cast.SelectMany(p => p.Item2));
+            sat = new DpllSolver<Applicant>(applicants_needing_alternative_cast.SelectMany(p => p.Item2));
             Solution solution = Solution.Unsolveable;
             Results.Clear();
             Initialise(applicants_needing_alternative_cast.Select(p => p.Item2.Count), out int chunk_size, out int max_chunks);
@@ -113,27 +77,7 @@ namespace Carmen.CastingEngine.SAT
                 if (!any_chunk_clause)
                     break; // we didn't have any chunk clauses, if this didn't solve, nothing will
             } while (Simplify(ref chunk_size, ref max_chunks));
-            // set alternative casts
-            List<IEnumerable<Assignment<Applicant>>> grouped_assignments;
-            if (!solution.IsUnsolvable)
-                // assigned by sat solver
-                grouped_assignments = sat.MapAssignments(solution)
-                    .GroupBy(a => a.Variable.CastGroup)
-                    .Select(g => g.AsEnumerable()).ToList();
-            else
-                // default to applicants evenly filled
-                grouped_assignments = applicants_needing_alternative_cast
-                    .Select(p => p.Item2.Select(a => new Assignment<Applicant>
-                    {
-                        Variable = a,
-                        Value = a.AlternativeCast?.Equals(alternativeCasts[1]) // keep the existing value if set
-                    })).ToList();
-            foreach (var group in grouped_assignments)
-            {
-                var full_assignments = EvenlyFillAssignments(group);
-                foreach (var assignment in full_assignments)
-                    assignment.Variable.AlternativeCast = alternativeCasts[assignment.Value!.Value ? 1 : 0];
-            }
+            return solution;
         }
 
         /// <summary>This is called to initialise the chunking parameters before we start SAT solving.
@@ -172,18 +116,6 @@ namespace Carmen.CastingEngine.SAT
             }
         }
 
-        /// <summary>Creates a clause specifying that all these applicants are in the same alternative cast</summary>
-        private IEnumerable<Clause<Applicant>> KeepTogether(Applicant[] applicants)
-        {
-            if (!cachedKeepTogether.TryGetValue(applicants.Length, out var builder))
-            {
-                builder = new ExpressionBuilder(applicants.Length, values => values.All(v => v) || values.All(v => !v));
-                cachedKeepTogether.Add(applicants.Length, builder);
-            }
-            foreach (var clause in builder.Apply(applicants).Clauses)
-                yield return clause;
-        }
-
         /// <summary>Creates a clause specifying that exactly half of the given applicants are in each alternative cast</summary>
         private IEnumerable<Clause<Applicant>> KeepSeparate(Applicant[] applicants)
         {
@@ -218,36 +150,6 @@ namespace Carmen.CastingEngine.SAT
                 return chunk;
             else
                 return null; // incomplete chunk
-        }
-
-        /// <summary>Fills any free (null) assignments, keeping the totals in each alternative cast as close to equal as possible</summary>
-        private Assignment<Applicant>[] EvenlyFillAssignments(IEnumerable<Assignment<Applicant>> assignments)
-        {
-            var result = assignments.OrderByDescending(a => ApplicantEngine.OverallAbility(a.Variable)).ToArray();
-            int count_true = 0;
-            int count_false = 0;
-            for (var r = 0; r < result.Length; r++)
-                if (result[r].Value is bool value)
-                {
-                    if (value)
-                        count_true++;
-                    else
-                        count_false++;
-                }
-            if (count_true + count_false != result.Length) // don't reiterate if no gaps
-                for (var r = 0; r < result.Length; r++)
-                    if (!result[r].Value.HasValue)
-                        if (count_true < count_false)
-                        {
-                            result[r].Value = true;
-                            count_true++;
-                        }
-                        else
-                        {
-                            result[r].Value = false;
-                            count_false++;
-                        }
-            return result;
         }
     }
 }
--- CastingEngine/SAT/SatEngine.cs
+++ CastingEngine/SAT/SatEngine.cs
@@ -20,9 +20,16 @@ namespace Carmen.CastingEngine.SAT
     /// </summary>
     public abstract class SatEngine : SelectionEngine
     {
-        public SatEngine(IApplicantEngine applicant_engine, AlternativeCast[] alternative_casts, Criteria? cast_number_order_by, ListSortDirection cast_number_order_direction)
+        protected Criteria[] primaryCriterias;
+
+        // expression building is O(2^n) and will be slow for large numbers, therefore cache
+        Dictionary<int, ExpressionBuilder> cachedKeepTogether = new();
+
+        public SatEngine(IApplicantEngine applicant_engine, AlternativeCast[] alternative_casts, Criteria? cast_number_order_by, ListSortDirection cast_number_order_direction, Criteria[] criterias)
             : base(applicant_engine, alternative_casts, cast_number_order_by, cast_number_order_direction)
-        { }
+        {
+            primaryCriterias = criterias.Where(c => c.Primary).ToArray();
+        }
 
         /// <summary>Select applicants into Cast Groups, respecting those already selected, by calculating the remaining count for each group,
         /// then taking the applicants with the highest overall ability until all cast groups are filled or there are no more applicants.
@@ -85,6 +92,112 @@ namespace Carmen.CastingEngine.SAT
                     return cg;
             }
             return null;
+        }
+
+        protected abstract Solution FindSatSolution(List<(CastGroup, HashSet<Applicant>)> applicants_needing_alternative_cast,
+            List<Clause<Applicant>> existing_assignments, List<Clause<Applicant>> same_cast_clauses, Dictionary<Applicant, SameCastSet> same_cast_lookup,
+            out Solver<Applicant> sat);
+
+        public override void BalanceAlternativeCasts(IEnumerable<Applicant> applicants, IEnumerable<SameCastSet> same_cast_sets)
+        {
+            if (alternativeCasts.Length != 2)
+                throw new ApplicationException("This approach only works for exactly 2 alternative casts.");
+            // sort the applicants into cast groups
+            var applicants_needing_alternative_cast = new List<(CastGroup, HashSet<Applicant>)>();
+            var existing_assignments = new List<Clause<Applicant>>();
+            foreach (var applicants_group in applicants.GroupBy(a => a.CastGroup))
+            {
+                if (applicants_group.Key is not CastGroup cast_group // not accepted into cast
+                    || !cast_group.AlternateCasts) // non-alternating cast group
+                {
+                    // alternative cast not required, set to null
+                    foreach (var applicant in applicants_group)
+                        applicant.AlternativeCast = null;
+                }
+                else
+                {
+                    var applicants_set = applicants_group.ToHashSet();
+                    applicants_needing_alternative_cast.Add((cast_group, applicants_set));
+                    // add clauses for applicants with alternative cast already set
+                    foreach (var applicant in applicants_set)
+                        if (applicant.AlternativeCast != null)
+                            existing_assignments.Add(Clause<Applicant>.Unit(applicant, applicant.AlternativeCast == alternativeCasts[1]));
+                }
+            }
+            // create clauses for same cast sets
+            var same_cast_clauses = new List<Clause<Applicant>>();
+            var same_cast_lookup = new Dictionary<Applicant, SameCastSet>();
+            foreach (var same_cast_set in same_cast_sets)
+            {
+                foreach (var applicant in same_cast_set.Applicants)
+                    same_cast_lookup.Add(applicant, same_cast_set); // Applicants can only be in one SameCastSet
+                same_cast_clauses.AddRange(KeepTogether(same_cast_set.Applicants.Where(a => applicants_needing_alternative_cast.Any(p => p.Item2.Contains(a))).ToArray()));
+            }
+            // run approach-specific SAT solving
+            var solution = FindSatSolution(applicants_needing_alternative_cast, existing_assignments, same_cast_clauses, same_cast_lookup, out var sat);
+            // set alternative casts
+            List<IEnumerable<Assignment<Applicant>>> grouped_assignments;
+            if (!solution.IsUnsolvable)
+                // assigned by sat solver
+                grouped_assignments = sat.MapAssignments(solution)
+                    .GroupBy(a => a.Variable.CastGroup)
+                    .Select(g => g.AsEnumerable()).ToList();
+            else
+                // default to applicants evenly filled
+                grouped_assignments = applicants_needing_alternative_cast
+                    .Select(p => p.Item2.Select(a => new Assignment<Applicant>
+                    {
+                        Variable = a,
+                        Value = a.AlternativeCast?.Equals(alternativeCasts[1]) // keep the existing value if set
+                    })).ToList();
+            foreach (var group in grouped_assignments)
+            {
+                var full_assignments = EvenlyFillAssignments(group);
+                foreach (var assignment in full_assignments)
+                    assignment.Variable.AlternativeCast = alternativeCasts[assignment.Value!.Value ? 1 : 0];
+            }
+        }
+
+        /// <summary>Creates a clause specifying that all these applicants are in the same alternative cast</summary>
+        private IEnumerable<Clause<Applicant>> KeepTogether(Applicant[] applicants)
+        {
+            if (!cachedKeepTogether.TryGetValue(applicants.Length, out var builder))
+            {
+                builder = new ExpressionBuilder(applicants.Length, values => values.All(v => v) || values.All(v => !v));
+                cachedKeepTogether.Add(applicants.Length, builder);
+            }
+            foreach (var clause in builder.Apply(applicants).Clauses)
+                yield return clause;
+        }
+
+        /// <summary>Fills any free (null) assignments, keeping the totals in each alternative cast as close to equal as possible</summary>
+        private Assignment<Applicant>[] EvenlyFillAssignments(IEnumerable<Assignment<Applicant>> assignments)
+        {
+            var result = assignments.OrderByDescending(a => ApplicantEngine.OverallAbility(a.Variable)).ToArray();
+            int count_true = 0;
+            int count_false = 0;
+            for (var r = 0; r < result.Length; r++)
+                if (result[r].Value is bool value)
+                {
+                    if (value)
+                        count_true++;
+                    else
+                        count_false++;
+                }
+            if (count_true + count_false != result.Length) // don't reiterate if no gaps
+                for (var r = 0; r < result.Length; r++)
+                    if (!result[r].Value.HasValue)
+                        if (count_true < count_false)
+                        {
+                            result[r].Value = true;
+                            count_true++;
+                        }
+                        else
+                        {
+                            result[r].Value = false;
+                            count_false++;
+                        }
+            return result;
         }
     }
 }
