15ca4b74661d457d830f870e2b1071bb93d50557
David Lang <davidlang42@gmail.com>
2021-09-14 17:07:16 +1000

    Fix random seeds

--- CastingEngine/Neural/Neuron.cs
+++ CastingEngine/Neural/Neuron.cs
@@ -5,17 +5,21 @@ namespace Carmen.CastingEngine.Neural
 {
     public class Neuron
     {
+        public const double MINIMUM_SEED = 0.2;
+        public const double MAXIMUM_SEED = 0.8;
         public InputFeature[] Inputs { get; set; }
         public double Bias { get; set; }
 
         public Neuron(int n_inputs)
         {
+            var random = new Random();
             Inputs = new InputFeature[n_inputs];
             for (var i = 0; i < Inputs.Length; i++)
                 Inputs[i] = new InputFeature
                 {
-                    Weight = new Random().NextDouble()
+                    Weight = random.NextDouble() * (MAXIMUM_SEED - MINIMUM_SEED) + MINIMUM_SEED
                 };
+            Bias = random.NextDouble() * (MAXIMUM_SEED - MINIMUM_SEED) + MINIMUM_SEED;
         }
 
         public double Calculate(double[] values)
