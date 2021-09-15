1474614b55e6b829c424b8bb512dd4ee03471412
David Lang <davidlang42@gmail.com>
2021-09-15 18:57:38 +1000

    Move conditions into neural network

--- CastingEngine/Neural/NeuralNetwork.cs
+++ CastingEngine/Neural/NeuralNetwork.cs
@@ -16,6 +16,15 @@ namespace Carmen.CastingEngine.Neural
     {
         FeedforwardNetwork network;
 
+        /// <summary>The maximum number of training iterations per data point</summary>
+        public int? MaxIterations { get; set; } = 10000;
+
+        /// <summary>The threshold of loss at which any lower value is considered success</summary>
+        public double? LossThreshold { get; set; } = 0.05;
+
+        /// <summary>The threshold of change in loss at which any lower value is considered a failure to change</summary>
+        public double? ChangeThreshold { get; set; } = 1e-10;
+
         public NeuralNetwork(int n_hidden_layers, int n_neurons_per_hidden_layer,
             IVectorActivationFunction? hidden_layer_activation = null,
             IVectorActivationFunction? output_layer_activation = null)
@@ -24,9 +33,42 @@ namespace Carmen.CastingEngine.Neural
                 IInputFeatureSet.GetSize<U>(), hidden_layer_activation, output_layer_activation);
         }
 
-        /// <summary>Train the model with a single input and expected output.
-        /// Returns the total loss prior to back propogation.</summary>
-        public double Train(T input, U expected_output) => network.Train(input.GetValues(), expected_output.GetValues());
+        /// <summary>Train the model with a set of inputs and outputs, using stochastic gradient decent</summary>
+        public Montage Train(IEnumerable<T> inputs, IEnumerable<U> expected_outputs)
+        {
+            if (MaxIterations == null && LossThreshold == null && ChangeThreshold == null)
+                throw new ApplicationException($"At least one of ({nameof(MaxIterations)}, {nameof(LossThreshold)}, {nameof(ChangeThreshold)}) must be set.");
+            var training_inputs = inputs.Select(i => i.GetValues()).ToArray();
+            var training_outputs = expected_outputs.Select(o => o.GetValues()).ToArray();
+            if (training_inputs.Length != training_outputs.Length)
+                throw new ArgumentException($"Length of {nameof(inputs)}[{training_inputs.Length}] must equal the length of {nameof(expected_outputs)}[{training_outputs.Length}].");
+            var repeat = 0;
+            var previous_loss = new double[training_inputs.Length];
+            var success = false;
+            var no_change = false;
+            var too_many_repeats = false;
+            while(!success && !no_change && !too_many_repeats)
+            {
+                success = true;
+                no_change = true;
+                for (var i = 0; i < training_inputs.Length; i++)
+                {
+                    var new_loss = network.Train(training_inputs[i], training_outputs[i]);
+                    no_change &= ChangeThreshold.HasValue && Math.Abs(new_loss - previous_loss[i]) < ChangeThreshold;
+                    previous_loss[i] = new_loss;
+                    success &= LossThreshold.HasValue && new_loss < LossThreshold;
+                }
+                too_many_repeats = ++repeat >= MaxIterations && MaxIterations.HasValue;
+            }
+            return new Montage
+            {
+                Success = success,
+                ReachedMaxIterations = too_many_repeats,
+                ReachedStableLoss = no_change,
+                Iterations = repeat,
+                FinalLoss = previous_loss
+            };
+        }
 
         public U Predict(T input)
         {
@@ -36,5 +78,16 @@ namespace Carmen.CastingEngine.Neural
         }
 
         public override string ToString() => network.ToString();
+    }
+
+    public struct Montage
+    {
+        public bool Success;
+        public bool ReachedMaxIterations;
+        public bool ReachedStableLoss;
+        public double[] FinalLoss;
+        public int Iterations;
+
+        public double AverageLoss => FinalLoss.Average();
     }
 }
