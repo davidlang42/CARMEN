e476a8a66c411c86c6d1ed87d6568796d7302dd8
David Lang <davidlang42@gmail.com>
2021-09-18 12:47:33 +1000

    Add SatTimePredictor project

---
+++ SatTimePredictor/Program.cs
@@ -0,0 +1,80 @@
+﻿using Carmen.CastingEngine.SAT.Internal;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SatTimePredictor
+{
+    class Program
+    {
+        const int MAX_VARS = 100;
+        const int MAX_CLAUSES = 1000;
+        const int MAX_LITERALS = 100;
+
+        static void Main(string[] args)
+        {
+            var filename = PromptFile("Filename to store SAT times?", $"sat_times_{DateTime.Now:yyyy-MM-dd_HH-mm-ss}.csv", false);
+            var start_seed = PromptInt("Starting at random seed?", 1);
+            var end_seed = PromptInt("Ending at random seed?", start_seed, start_seed);
+            var start_n_vars = PromptInt("Starting at N variables?", 1, 1, MAX_VARS);
+            var end_n_vars = PromptInt("Ending at N variables?", Math.Min(start_n_vars + 99, MAX_VARS), start_n_vars, MAX_VARS);
+            var start_j_clauses = PromptInt("Starting at J clauses?", 1, 1, MAX_CLAUSES);
+            var end_j_clauses = PromptInt("Ending at J clauses?", Math.Min(start_j_clauses + 99, MAX_CLAUSES), start_j_clauses, MAX_CLAUSES);
+            var start_k_literals = PromptInt("Starting at K literals per clause?", 1, 1, MAX_LITERALS);
+            var end_k_literals = PromptInt("Ending at K literals per clause?", Math.Min(start_k_literals + 9, MAX_LITERALS), start_k_literals, MAX_LITERALS);
+            var timer = new SatTimer(start_n_vars, end_n_vars, start_j_clauses, end_j_clauses, start_k_literals, end_k_literals);
+            var percent_delta = 100.0 / timer.TotalCombinations;
+            using var f = new StreamWriter(filename);
+            f.WriteLine(SatResult.ToHeader());
+            for (var seed = start_seed; seed <= end_seed; seed++)
+            {
+                var percent = 0.0;
+                Console.WriteLine($"########## Starting pass for seed: {seed}/{end_seed} ##########");
+                foreach (var result in timer.Run(seed))
+                {
+                    f.WriteLine(result.ToString());
+                    percent += percent_delta;
+                    Console.WriteLine($"[{percent:0.00}%] ({result.NVariables}n, {result.JClauses}j, {result.KLiterals}k) => {result.SecondsTaken:0.000}s ({result.Solutions} solutions)");
+                }
+            }
+            Console.WriteLine("########## COMPLETE ##########");
+        }
+
+        private static string PromptFile(string prompt, string default_value, bool existing_file)
+        {
+            while (true)
+            {
+                Console.Write($"{prompt} [{default_value}] ");
+                var input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                    return default_value;
+                else if (!existing_file && File.Exists(input))
+                    Console.WriteLine("File already exists, choose a new file.");
+                else if (existing_file && !File.Exists(input))
+                    Console.WriteLine("File does not exist, choose an existing file.");
+                else
+                    return input;
+            }
+        }
+
+        private static int PromptInt(string prompt, int default_value, int? min = null, int? max = null)
+        {
+            while (true)
+            {
+                Console.Write($"{prompt} [{default_value}] ");
+                var input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                    return default_value;
+                else if (!int.TryParse(input, out var result))
+                    Console.WriteLine("Input must be an integer.");
+                else if (min.HasValue && result < min.Value)
+                    Console.WriteLine($"Input must be at least {min}.");
+                else if (max.HasValue && result > max.Value)
+                    Console.WriteLine($"Input cannot be more than {max}.");
+                else
+                    return result;
+            }
+        }
+    }
+}
---
+++ SatTimePredictor/SatTimer.cs
@@ -0,0 +1,90 @@
+﻿using Carmen.CastingEngine.SAT.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SatTimePredictor
+{
+    class SatTimer
+    {
+        int nStart, nEnd, jStart, jEnd, kStart, kEnd;
+
+        public int TotalCombinations => (nEnd - nStart + 1) * (jEnd - jStart + 1) * (kEnd - kStart + 1);
+
+        public SatTimer(int start_n_vars, int end_n_vars, int start_j_clauses, int end_j_clauses, int start_k_literals, int end_k_literals)
+        {
+            this.nStart = start_n_vars;
+            this.nEnd = end_n_vars;
+            this.jStart = start_j_clauses;
+            this.jEnd = end_j_clauses;
+            this.kStart = start_k_literals;
+            this.kEnd = end_k_literals;
+        }
+
+        public IEnumerable<SatResult> Run(int random_seed)
+        {
+            for (var n = nStart; n <= nEnd; n++)
+                for (var j = jStart; j <= jEnd; j++)
+                    for (var k = kStart; k <= kEnd; k++)
+                    {
+                        var result = new SatResult
+                        {
+                            RandomSeed = random_seed,
+                            NVariables = n,
+                            JClauses = j,
+                            KLiterals = k,
+                            SecondsTaken = TimeToSolve(random_seed, n, j, k, out var solutions)
+                        };
+                        result.Solutions = solutions.Length;
+                        yield return result;
+                    }
+        }
+
+        private double TimeToSolve(int random_seed, int n_variables, int j_clauses, int k_literals_per_clause, out Solution[] solution)
+        {
+            var vars = Enumerable.Range(1, n_variables).ToArray();
+            var sat = new DpllAllSolver<int>(vars);
+            var expression = GenerateExpression(random_seed, vars, j_clauses, k_literals_per_clause);
+            var start = DateTime.Now;
+            solution = sat.Solve(expression).ToArray();
+            var duration = DateTime.Now - start;
+            return duration.TotalSeconds;
+        }
+
+        public static Expression<T> GenerateExpression<T>(int random_seed, T[] variables, int j_clauses, int k_literals_per_clause)
+            where T : notnull
+        {
+            var random = new Random(random_seed);
+            var all_literals = variables.SelectMany(v => new[] { Literal<T>.Positive(v), Literal<T>.Negative(v) }).ToArray();
+            var clauses = new List<Clause<T>>();
+            for (var j = 0; j < j_clauses; j++)
+            {
+                var literals = new List<Literal<T>>();
+                for (var k = 0; k < k_literals_per_clause; k++)
+                {
+                    literals.Add(all_literals[random.Next(all_literals.Length)]);
+                }
+                clauses.Add(new Clause<T> { Literals = literals.ToHashSet() });
+            }
+            return new Expression<T> { Clauses = clauses.ToHashSet() };
+        }
+    }
+
+    public struct SatResult
+    {
+        public int RandomSeed;
+        public int NVariables;
+        public int JClauses;
+        public int KLiterals;
+        public double SecondsTaken;
+        public int Solutions;
+
+        public static string ToHeader()
+            => $"{nameof(RandomSeed)},{nameof(NVariables)},{nameof(JClauses)},{nameof(KLiterals)},{nameof(SecondsTaken)},{nameof(Solutions)}";
+
+        public override string ToString()
+            => $"{RandomSeed},{NVariables},{JClauses},{KLiterals},{SecondsTaken},{Solutions}";
+    }
+}
