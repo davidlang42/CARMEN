e939e579e4ea5cc473cc8eb9585ebbe924fe0a8c
David Lang <davidlang42@gmail.com>
2021-09-29 17:48:04 +1000

    Use ClassificationError{0.5} (~0.1% better on every case)

--- CastingEngine/Neural/NeuralAllocationEngine.cs
+++ CastingEngine/Neural/NeuralAllocationEngine.cs
@@ -61,7 +61,7 @@ namespace Carmen.CastingEngine.Neural
             // Construct the model
             this.confirm = confirm;
             nInputs = (suitabilityRequirements.Length + existingRoleRequirements.Length) * 2;
-            model = new SingleLayerPerceptron(nInputs, 1, new Sigmoid(), new MeanSquaredError());
+            model = new SingleLayerPerceptron(nInputs, 1, new Sigmoid(), new ClassificationError { Threshold = 0.5 });
             LoadWeights();
         }
 
