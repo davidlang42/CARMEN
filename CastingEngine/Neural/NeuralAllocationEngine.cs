5484f00d421048983f0c9d5fc35737cbee312b6f
David Lang <davidlang42@gmail.com>
2021-10-03 11:24:06 +1100

    Default classification error threshold of 0.4

--- CastingEngine/Neural/NeuralAllocationEngine.cs
+++ CastingEngine/Neural/NeuralAllocationEngine.cs
@@ -63,7 +63,7 @@ namespace Carmen.CastingEngine.Neural
             // Construct the model
             this.confirm = confirm;
             nInputs = (suitabilityRequirements.Length + existingRoleRequirements.Length + 1) * 2;
-            model = new SingleLayerPerceptron(nInputs, 1, new Sigmoid(), new ClassificationError { Threshold = 0.5 }); //TODO vary classification error threshold if required
+            model = new SingleLayerPerceptron(nInputs, 1, new Sigmoid(), new ClassificationError { Threshold = 0.4 });
             LoadWeights();
         }
 
