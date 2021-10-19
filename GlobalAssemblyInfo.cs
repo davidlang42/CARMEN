143496ccdbd1aeccea65dfc86b55d6db14b84608
David Lang <davidlang42@gmail.com>
2021-10-19 23:24:03 +1100

    Bump version to 0.5

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,13 +1,13 @@
 ﻿using System.Reflection;
 
 // AssemblyInformationalVersion is used for Application.ProductVersion
-[assembly: AssemblyInformationalVersion("0.4"
+[assembly: AssemblyInformationalVersion("0.5"
 #if DEBUG
     + "-debug"
 #endif
     )]
 // AssemblyVersion must be deterministic in debug mode so that user settings persist
-[assembly: AssemblyVersion("0.4"
+[assembly: AssemblyVersion("0.5"
 #if !DEBUG
     + ".*"
 #endif
