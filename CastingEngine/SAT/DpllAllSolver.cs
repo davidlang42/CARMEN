263d5968b2d59bfb337b07633c35f8335100be7f
David Lang <davidlang42@gmail.com>
2021-10-23 22:38:08 +1100

    Parallelise All-solvers (19s -> 15s, All_50_Vars, but most of that is checking the solutions)

--- CastingEngine/SAT/DpllAllSolver.cs
+++ CastingEngine/SAT/DpllAllSolver.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Carmen.CastingEngine.SAT
 {
@@ -18,14 +20,17 @@ namespace Carmen.CastingEngine.SAT
 
         protected override IEnumerable<Solution> BranchPartialSolve(HashSet<Clause<int>> remaining_clauses, Clause<int>[] branching_clauses, Solution partial_solution)
         {
-            foreach (var branching_clause in branching_clauses)
+            var solutions = new ConcurrentBag<Solution>();
+            Parallel.ForEach(branching_clauses, branching_clause =>
             {
                 var branch_clauses = new HashSet<Clause<int>>(remaining_clauses);
                 branch_clauses.Add(branching_clause);
                 var new_expression = new Expression<int>(branch_clauses);
                 foreach (var solution in PartialSolve(new_expression, partial_solution))
-                    yield return solution; // propogate any found solutions
-            }
+                    solutions.Add(solution);
+            });
+            foreach (var solution in solutions)
+                yield return solution;
         }
     }
 }
