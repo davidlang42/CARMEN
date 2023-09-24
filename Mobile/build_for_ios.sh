bbb53fe6e7c5e99de1638b236e038974e4d796f4
David Lang <davidlang42@gmail.com>
2023-09-24 12:22:13 +1000

    Fix

--- Mobile/build_for_ios.sh
+++ Mobile/build_for_ios.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 set -e
 dotnet publish -f net7.0-ios -c Release
+echo BUILT FOR AD-HOC DEPLOYMENT
+echo Distribute using Apple Configurator: https://apps.apple.com/app/id1037126344
+echo User guide: https://support.apple.com/en-au/guide/apple-configurator-mac/welcome/mac
