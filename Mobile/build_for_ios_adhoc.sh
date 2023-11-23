e258d9a6dbfc99f542fe516006530a14f585c73a
David Lang <davidlang42@gmail.com>
2023-11-24 00:49:47 +1100

    Set up iOS build for app store

--- Mobile/build_for_ios_adhoc.sh
+++ Mobile/build_for_ios_adhoc.sh
@@ -2,7 +2,7 @@
 set -e
 rm -r bin
 rm -r obj
-dotnet publish -f net7.0-ios -c Release
+dotnet publish -f net7.0-ios -c Release -p:CodesignProvision="CARMEN (Ad-Hoc)"
 echo BUILT FOR AD-HOC DEPLOYMENT
 echo Distribute using Apple Configurator: https://apps.apple.com/app/id1037126344
 echo User guide: https://support.apple.com/en-au/guide/apple-configurator-mac/welcome/mac
---
+++ Mobile/build_for_ios_appstore.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+rm -r bin
+rm -r obj
+dotnet publish -f net7.0-ios -c Release -p:CodesignProvision="CARMEN (App Store)"
+IPA_FILE="`ls -1 ./bin/Release/net7.0-ios/ios-arm64/publish/Carmen.Mobile*.ipa | tail -n 1`"
+echo Press ENTER to submit to Apple AppStore: $IPA_FILE
+read
+xcrun altool --upload-app -f $IPA_FILE -t ios --username davidlang42@gmail.com --apple-id davidlang42@gmail.com # this will prompt for an AppleID app specific password
+# could use Transporter instead of xcrun: https://apps.apple.com/us/app/transporter/id1450874784?mt=12
