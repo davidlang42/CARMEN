537f6e4ca382f18ed3ce902f48017e72c46315fd
David Lang <davidlang42@gmail.com>
2024-01-12 17:50:42 +1100

    Disable parallel button when not relevant

--- Desktop/Pages/AllocateRoles.xaml.cs
+++ Desktop/Pages/AllocateRoles.xaml.cs
@@ -130,7 +130,6 @@ namespace Carmen.Desktop.Pages
         private void CancelButton_Click(object sender, RoutedEventArgs e)
             => ChangeToViewMode();
 
-        //TODO (OR ADD ISSUE) Change error message "Parallel casting is only applicable to sections which don't allow applicants to have multiple roles within them." into a disabled button with a tooltip
         //TODO (OR ADD ISSUE) Revise how IdealCastingOrder works, so that it recommends using parallel casting for high (but similar) priority roles within single-role sections
         //TODO (OR ADD ISSUE) Speed up line generation: generate lines only when IsSelected is true, keep a reference to them, remove them when IsSelected is false
         private async void SaveButton_Click(object sender, RoutedEventArgs e)
