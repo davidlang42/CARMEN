db92a8c0e131dd71ef6b6253d7b548c8705b285f
davidlang42 <37281992+davidlang42@users.noreply.github.com>
2024-09-22 22:36:48 +1000

    Untracked: Upgrade to .net8 (#75)

--- Mobile/build_for_ios_appstore.sh
+++ Mobile/build_for_ios_appstore.sh
@@ -2,8 +2,8 @@
 set -e
 rm -r bin
 rm -r obj
-dotnet publish -f net7.0-ios -c Release -p:CodesignProvision="CARMEN (App Store)"
-IPA_FILE="`ls -1 ./bin/Release/net7.0-ios/ios-arm64/publish/Carmen.Mobile*.ipa | tail -n 1`"
+dotnet publish -f net8.0-ios -c Release -p:CodesignProvision="CARMEN (App Store)"
+IPA_FILE="`ls -1 ./bin/Release/net8.0-ios/ios-arm64/publish/Carmen.Mobile*.ipa | tail -n 1`"
 echo Press ENTER to submit to Apple AppStore: $IPA_FILE
 read
 xcrun altool --upload-app -f $IPA_FILE -t ios --username davidlang42@gmail.com --apple-id davidlang42@gmail.com # this will prompt for an AppleID app specific password
