84106060ab1bdc888d5a544c8ce14dc495e69254
David Lang <davidlang42@gmail.com>
2024-11-29 21:21:47 +1100

    Revert "Untracked: Upgrade to .net8 (#75)"
    This reverts commit db92a8c0e131dd71ef6b6253d7b548c8705b285f.

--- Mobile/build_for_mac.sh
+++ Mobile/build_for_mac.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
-dotnet publish -f net8.0-maccatalyst -c Release
-PKG_FILE="`ls -1 ./bin/Release/net8.0-maccatalyst/publish/Carmen.Mobile*.pkg | tail -n 1`"
+dotnet publish -f net7.0-maccatalyst -c Release
+PKG_FILE="`ls -1 ./bin/Release/net7.0-maccatalyst/publish/Carmen.Mobile*.pkg | tail -n 1`"
 echo Press ENTER to submit to Apple: $PKG_FILE
 read
 xcrun notarytool submit $PKG_FILE --wait --apple-id davidlang42@gmail.com --team-id 45435R99CA # this will prompt for an AppleID app specific password
