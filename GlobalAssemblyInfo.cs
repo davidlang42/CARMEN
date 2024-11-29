0685f1d6727169c4bae2b3ac01796828ff038e50
davidlang42 <37281992+davidlang42@users.noreply.github.com>
2024-11-29 23:12:34 +1100

    Untracked: Bump version (#79)

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,13 +1,13 @@
 ﻿using System.Reflection;
 
 // AssemblyInformationalVersion is used for Application.ProductVersion
-[assembly: AssemblyInformationalVersion("1.7"
+[assembly: AssemblyInformationalVersion("1.8"
 #if DEBUG
     + "-debug"
 #endif
     )]
 // AssemblyVersion must be deterministic in debug mode so that user settings persist
-[assembly: AssemblyVersion("1.7"
+[assembly: AssemblyVersion("1.8"
 #if !DEBUG
     + ".*"
 #endif
